3345e1be6c4645623a8ef86caf856704887aba34
@ayushi_imenso <aagrawal@imenso.tech>
2023-04-21 13:25:01 +0530

    initial changes

---
+++ test/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// import xyz from "test/xyz"
+// const myFile = require('test/xyz.js');
+// const myFile = require('./xyz');
+
+
+function App() {
+  // const add= myFile.sum(10,100)
+  return (
+    <div>
+     <h1>Hello</h1>
+   {/* <h2>Addition is: {add}</h2> */}
+    </div>
+  );
+}
+
+export default App;
